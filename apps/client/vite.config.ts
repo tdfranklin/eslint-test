85392656e9113bdd02cfe03639738742ad47b3fb
Tyler D. Franklin <irishlep84@gmail.com>
2023-05-17 13:47:45 -0500

    first commit

---
+++ apps/client/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default () =>
+  defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      port: 1234,
+    },
+    preview: {
+      port: 3050,
+    },
+    build: {
+      sourcemap: true,
+    },
+  })
